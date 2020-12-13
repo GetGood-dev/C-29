322795f478cf669bc171dc0f19b02c7a22361405
GetGood-dev <74246470+GetGood-dev@users.noreply.github.com>
2020-12-13 17:06:21 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(r,g,b)
+  {
+    var pos =this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    fill(r,g,b);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
